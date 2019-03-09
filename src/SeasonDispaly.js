59393d72bc7dfcaba803fe0dfc4df72f65ece37a
ahouzi <mr.ahouzi@gmail.com>
2019-03-09 14:28:08 +0100

    season app is done!

---
+++ src/SeasonDispaly.js
@@ -0,0 +1,46 @@
+import './SeasonDisplay.css'
+import React from 'react';
+
+
+const seasonConfig = {
+
+    summer:{
+        text: "Let's hit the beach",
+        iconName: 'sun'
+    },
+    winter:{
+        text: "Burr, it's chilly",
+        iconName: 'snowflake'
+    }
+
+};
+
+
+const getSeason = (lat, month)=>{
+if (month>2 && month<9){
+   return lat > 0 ? 'summer' : 'winter';
+}
+else {
+    return lat > 0 ? 'winter' : 'summer';
+}
+
+};
+
+const SeasonDispaly = (props)=> {
+    const season = getSeason(props.lat,new Date().getMonth());
+
+    const {text, iconName} = seasonConfig[season];
+
+    return (
+      <div className={`season-display ${season}`}>
+          <i className={`icon-top-left massive ${iconName} icon`} />
+
+          <h1>{text}</h1>
+          <i className={`icon-bottom-right massive ${iconName} icon`} />
+      </div>
+  );
+
+};
+
+
+export default SeasonDispaly;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,55 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import SeasonDispaly from "./SeasonDispaly";
+import Spinner from "./Spinner";
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+class App extends React.Component{
+
+
+    state={ lat : undefined , errorMsg : ''};
+
+    componentDidMount() {
+        console.log('My component was rendered to the screen');
+
+        window.navigator.geolocation.getCurrentPosition(
+            position => this.setState({lat : position.coords.latitude}),
+            err => this.setState({errorMsg : err.message})
+        );
+
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        console.log('My component was just updated - it re-rendered!');
+    }
+
+
+    renderContent(){
+
+        if (this.state.errorMsg && !this.state.lat) {
+            return <div>Error : {this.state.errorMsg}</div>;
+        }
+
+        if (!this.state.errorMsg && this.state.lat) {
+            return <SeasonDispaly lat = {this.state.lat}/>;
+        }
+
+        return <Spinner/>;
+    }
+
+
+
+    // must have function
+    render() {
+        return (
+            <div>{this.renderContent()}</div>
+        );
+    }
+}
+
+
+ReactDOM.render(
+    <App/>,
+    document.querySelector("#root")
+);
