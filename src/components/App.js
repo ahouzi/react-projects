469721631011a838c2dc6f3dea021aa2ed94e80e
ahouzi <mr.ahouzi@gmail.com>
2019-03-10 15:46:55 +0100

    fix setState bug

--- src/components/App.js
+++ src/components/App.js
@@ -6,24 +6,26 @@ import Axios from "axios";
 
 class App extends React.Component{
 
-    async onSearchSubmit(term) {
+    state = {images : []};
 
-       const response = await Axios.get('https://api.unsplash.com/search/photos',{
-            headers : {
-                Authorization: 'Client-ID fb14897f1610e74ad86c8a338827b3d8f1195e1a227273ca77313649bf284a65'
-            },
+     onSearchSubmit =  async (term) => {
+         const response = await Axios.get('https://api.unsplash.com/search/photos', {
+             headers: {
+                 Authorization: 'Client-ID fb14897f1610e74ad86c8a338827b3d8f1195e1a227273ca77313649bf284a65'
+             },
 
-            params : {query : term }
+             params: {query: term}
 
-        });
+         });
 
-       console.log(response.data.results);
-    }
+         this.setState({images: response.data.results});
+     }
 
     render() {
         return (
             <div className="ui container" style={{marginTop : '20px'}}>
                 <SearchBar handleSubmit ={this.onSearchSubmit}/>
+                Found {this.state.images.length}  images
             </div>
         );
     }
